4bb3442ef52e28cc3ac6f2eaa76c8b5026093ec1
Alexandr Mikhailik <alex-under-2010@mail.ru>
2017-02-20 00:17:20 +0200

    Physics, shooting, flying added

---
+++ Assets/Scripts/BulletShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletShoot : MonoBehaviour
+{
+    public Rigidbody2D bullet;
+    public float speed = 1;
+    public Button Fire;
+
+    public void Shoot()
+    {
+        Rigidbody2D fire = Instantiate(bullet, transform.position, transform.rotation);
+        fire.velocity = transform.TransformDirection(new Vector3(speed, 0, 0));
+        Destroy(fire.gameObject, 0.8f);
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        Rigidbody2D fire = Instantiate(bullet, transform.position, transform.rotation);
+	        fire.velocity = transform.TransformDirection(new Vector3(speed, 0, 0));
+            Destroy(fire.gameObject, 0.8f);
+        }
+
+    }
+    
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private int RotSpeed = 150;
+
+    public float ForceStrength;
+
+    public bool isPressed = false;
+    public bool isNotePressed = false;
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isPressed)
+        {
+            player.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * ForceStrength, ForceMode2D.Force);
+            player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 0.02f, ForceMode2D.Impulse);
+        }
+
+        else if (!isPressed)
+        {
+
+            //nothing
+        }
+
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            player.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * ForceStrength, ForceMode2D.Force);
+            player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 0.01f, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            player.GetComponent<Rigidbody2D>().AddRelativeForce(-Vector2.right * 0.2f, ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.acceleration.x < -0.15)
+        {
+            player.transform.Rotate(Vector3.forward * RotSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || Input.acceleration.x > 0.15)
+        {
+            player.transform.Rotate(-Vector3.forward * RotSpeed * Time.deltaTime);
+        }
+    }
+    public void onPointerDownButton()
+    {
+        isPressed = true;
+    }
+    public void onPointerUpButton()
+    {
+        isPressed = false;
+    }
+}
