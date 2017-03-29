e44f82aacb8fe6500249ae7d7f9a2ec69b567ee0
Alexandr Mikhailik <alex-under-2010@mail.ru>
2017-03-29 13:57:23 +0300

    fixed timer, added Score Scene

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -1,24 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour {
 
+
+
+
     // Use this for initialization
-    public float timeLeft = 150.0f;
+    public float timeLeft = 10.0f;
 
     public Text TimerText;
 
-
+    private bool flag = true;
 
     void Update()
     {
-        timeLeft -= Time.deltaTime;
-        TimerText.text = Mathf.Round(timeLeft).ToString();
-        if (timeLeft < 0)
+        if (flag)
         {
-            Debug.Log("EndGame");
+            timeLeft -= Time.deltaTime;
+            TimerText.text = Mathf.Round(timeLeft).ToString();
+
+            if (timeLeft <= 0)
+            {
+                flag = false;
+                PlayerPrefs.SetInt("Score", Convert.ToInt32(TimerText.text));
+            }
         }
     }
 }
