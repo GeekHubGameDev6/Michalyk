987e8c42d026152ccae989a1731e4866dd1e2ebb
Alexandr Mikhailik <alex-under-2010@mail.ru>
2017-03-30 22:54:40 +0300

    added levels, score scene improvements, fixes

---
+++ Assets/Scripts/Game/Levels/CountdownTimerHard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CountdownTimerHard : MonoBehaviour {
+
+    // Use this for initialization
+    public float timeLeft = 10.0f;
+
+    public Text TimerText;
+
+    public Text Points;
+
+    private bool flag = true;
+
+    void Update()
+    {
+        if (flag)
+        {
+            timeLeft -= Time.deltaTime;
+            TimerText.text = Mathf.Round(timeLeft).ToString();
+
+            if (timeLeft <= 0)
+            {
+                flag = false;
+                PointsCalculate();
+                SceneManager.LoadScene("HighScores");
+            }
+        }
+    }
+
+    void PointsCalculate()
+    {
+        var Score = Convert.ToInt32(Points.text);
+        PlayerPrefs.SetInt("ScoreHard", Score);
+        var ScoreHardTop = PlayerPrefs.GetInt("ScoreHardTop");
+
+        if (Score > ScoreHardTop)
+        {
+            PlayerPrefs.SetInt("ScoreHardTop", Score);
+        }
+        PlayerPrefs.Save();
+    }
+}
---
+++ Assets/Scripts/Game/Levels/CountdownTimerMedium.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CountdownTimerMedium : MonoBehaviour {
+
+    // Use this for initialization
+    public float timeLeft = 10.0f;
+
+    public Text TimerText;
+
+    public Text Points;
+
+    private bool flag = true;
+
+    void Update()
+    {
+        if (flag)
+        {
+            timeLeft -= Time.deltaTime;
+            TimerText.text = Mathf.Round(timeLeft).ToString();
+
+            if (timeLeft <= 0)
+            {
+                flag = false;
+                PointsCalculate();
+                SceneManager.LoadScene("HighScores");
+            }
+        }
+    }
+
+    void PointsCalculate()
+    {
+        var Score = Convert.ToInt32(Points.text);
+        PlayerPrefs.SetInt("ScoreMedium", Score);
+        var ScoreMediumTop = PlayerPrefs.GetInt("ScoreMediumTop");
+
+        if (Score > ScoreMediumTop)
+        {
+            PlayerPrefs.SetInt("ScoreMediumTop", Score);
+        }
+        PlayerPrefs.Save();
+    }
+}
